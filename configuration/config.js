4ffa7756ac7f7f6999e22fa0fa5c2ac7f997cd12
Leslie Cheng <leslie.cheng5@gmail.com>
2019-10-19 15:43:11 -0700

    Removed configuration-specific files.

--- configuration/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  JWT_SECRET: "evntauth",
-  facebook: {
-    clientID: process.env.FB_CLIENT_ID,
-    clientSecret: process.env.FB_CLIENT_SECRET
-  }
-};

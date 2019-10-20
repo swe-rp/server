e8fc552771a9ec2e1298fe4dd561d73326efb852
Alberto Misail <ajmisailguber@gmail.com>
2019-10-19 17:27:46 -0700

    Refactoring backend

--- models/event.js
+++ models/event.js
@@ -6,16 +6,47 @@ var eventSchema = mongoose.Schema;
 eventSchema = mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
-    visibility: {type: Boolean, required: true},
-    host_list: [{ type: mongoose.Schema.Types.ObjectId, ref: model_names.USER} ],
-    guest_list: [{ type: mongoose.Schema.Types.ObjectId, ref: model_names.USER} ],
+    // visibility: {type: Boolean, required: true},
+    // host_list: [{ type: mongoose.Schema.Types.ObjectId, ref: model_names.USER} ],
+    host: { type: mongoose.Schema.Types.ObjectId, ref: model_names.USER},
+    // guest_list: [{ type: mongoose.Schema.Types.ObjectId, ref: model_names.USER} ],
     attendants_list: [{ type: mongoose.Schema.Types.ObjectId, ref: model_names.USER} ],
-    location_x: {type: Number},
-    location_y: {type: Number},
+    // location_x: {type: Number},
+    // location_y: {type: Number},
     start_time: {type: Date},
     end_time: {type: Date},
     tag_list: [{ type: String} ],
 });
 
-var Event = mongoose.model(model_names.EVENT, eventSchema);
-module.exports = Event;
+var EventModel = mongoose.model(model_names.EVENT, eventSchema);
+
+async function createEvent(body){
+    const newEvent = new Event({
+        name: body.name,
+        description: body.description,
+        host: body.host,
+        attendants_list: [],
+        start_time: body.start_time,
+        end_time: body.end_time
+    });
+    await newEvent.save()
+    return ({ "id": newEvent.id, "data": newEvent });
+}
+
+async function updateEvent(id, body){
+    var query = { _id: id }
+    var update = {
+        name: body.name,
+        description: body.description,
+        host: body.host,
+        start_time: body.start_time,
+        end_time: body.end_time
+    }
+    var updated = await EventModel.findOneAndUpdate(query, update);
+    return ({ "id": updated.id, "data": updated });
+}
+
+module.exports = {
+    "createEvent": createEvent,
+    "updateEvent": updateEvent
+};
--- routes/events.js
+++ routes/events.js
@@ -3,167 +3,153 @@ const router = express.Router();
 
 var admin = require('firebase-admin');
 
-const EventModel = require('../models/event');
+const Event = require('../models/event');
 const UserModel = require('../models/user');
 
 // Event api
 // Create event
 router.post('/', async function (req, res) {
-    try {
-        const newEvent = new EventModel({
-            name: req.body.name,
-            description: req.body.description,
-            visibility: req.body.visibility,
-            // location_x: req.body.location_x,
-            // location_y: req.body.location_y,
-            // start_time: req.body.start_time,
-            // end_time: req.body.end_time,
-        });
-        await newEvent.save();
-        var registrationTokens = [];
-        newEvent.host_list.forEach(async (host_id) => {
-            var token = (await UserModel.findById(host_id)).registrationToken;
-            registrationTokens.push(token);
-        });
-        //TODO fix this
-        admin.messaging().subscribeToTopic(registrationTokens, newEvent._id)
-            .then(function (response) {
-                // See the MessagingTopicManagementResponse reference documentation
-                // for the contents of response.
-                console.log(`Successfully subscribed to topic: ${newEvent._id}`, response);
-            })
-            .catch(function (error) {
-                console.log(`Error subscribing to topic: ${newEvent._id}`, error);
-            });
+    try{
+        const newEvent = Event.createEvent(req.body);
         res.status(200).json(newEvent);
     } catch (err) {
         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
     }
 });
 
-router.get('/', async function (req, res) {
-    try {
-        var allEvents = await EventModel.find();
-        res.status(200).json(allEvents);
-    } catch (err) {
-        res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
-    }
-})
+//Edit event
+router.put('/:id', async function (req, res) {
+    try{
 
-// Edit event, changed public/private, changed time or location
-router.put('/:user_id', async function (req, res) {
-    var user_id = req.params.user_id;
-    try {
-        var query = {
-            $and: [{ _id: req.body._id }, { host_list: user_id }]
-        }
-        var update = {
-            name: req.body.name,
-            description: req.body.description,
-            visibility: req.body.visibility,
-            // location_x: req.body.location_x,
-            // location_y: req.body.location_y,
-            // start_time: req.body.start_time,
-            // end_time: req.body.end_time
-        }
-        await EventModel.findOneAndUpdate(query, update);
-        var message = {
-            data: {
-                content: `${update.name} event was updated`
-            },
-            topic: req.body._id
-        }
-        admin.messaging().send(message)
-            .then((response) => {
-                // Response is a message ID string.
-                console.log(`Successfully sent message to topic: ${message.topic}`, response);
-            })
-            .catch((error) => {
-                console.log(`Error sending message to topic: ${message.topic}`, error);
-            });
-        res.status(200);
-    } catch (err) {
+        res.status(200).json(updatedEvent);
+    }catch (err) {
         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
     }
 });
 
-// // Delete event
-router.delete('/:user_id', async function (req, res) {
-    var user_id = req.params.user_id;
-    try {
-        var query = {
-            $and: [{ _id: req.body._id }, { host_list: user_id }]
-        }
-        await EventModel.remove(query);
-        //TODO notify on delete
-        res.status(200);
-    } catch (err) {
-        res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
-    }
-});
+// router.get('/', async function (req, res) {
+//     try {
+//         var allEvents = await EventModel.find();
+//         res.status(200).json(allEvents);
+//     } catch (err) {
+//         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
+//     }
+// })
 
-// // Get suggested event for user
-router.get('/suggest/:user_id', async function (req, res) {
-    var user_id = req.params.user_id;
-    try {
-        var user = await UserModel.findOne({ facebook_id: user_id }).populate(attended_events_list);
-        var tagFreq = {};
-        user.attended_events_list.forEach(event => {
-            event.tag_list.forEach(tag => {
-                if (!tagFreq[tag])
-                    tagFreq[tag] = 0;
-                tagFreq[tag] = tagFreq[tag] + 1;
-            });
-        });
-        var events = getVisibleEventsForUser();
-        if (events.length == 0)
-            res.status(409).send({ success: false, message: "no events for you" });
-        var best_event = events[0];
-        var best_score = getEventScoreForUser(tagFreq, user.friends_list, events[0])
-        for (var i = 1; i < events.length; i++) {
-            var score = getEventScoreForUser(tagFreq, user.friends_list, events[i]);
-            if (score > best_score) {
-                best_score = score;
-                best_event = events[i];
-            }
-        }
-        res.status(200).json(best_event);
-    } catch (err) {
-        res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
-    }
-});
+// // Edit event, changed public/private, changed time or location
+// router.put('/:user_id', async function (req, res) {
+//     var user_id = req.params.user_id;
+//     try {
+//         var query = {
+//             $and: [{ _id: req.body._id }, { host_list: user_id }]
+//         }
+//         var update = {
+//             name: req.body.name,
+//             description: req.body.description,
+//             visibility: req.body.visibility,
+//             // location_x: req.body.location_x,
+//             // location_y: req.body.location_y,
+//             // start_time: req.body.start_time,
+//             // end_time: req.body.end_time
+//         }
+//         await EventModel.findOneAndUpdate(query, update);
+//         var message = {
+//             data: {
+//                 content: `${update.name} event was updated`
+//             },
+//             topic: req.body._id
+//         }
+//         admin.messaging().send(message)
+//             .then((response) => {
+//                 // Response is a message ID string.
+//                 console.log(`Successfully sent message to topic: ${message.topic}`, response);
+//             })
+//             .catch((error) => {
+//                 console.log(`Error sending message to topic: ${message.topic}`, error);
+//             });
+//         res.status(200);
+//     } catch (err) {
+//         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
+//     }
+// });
 
-// Get all events for user, used in browse events
-router.get('/:user_id', function (req, res) {
-    var user_id = req.params.user_id;
-    try {
-        var events = getVisibleEventsForUser(user_id);
-        res.status(200).json(events);
-    } catch (err) {
-        res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
-    }
-});
+// // // Delete event
+// router.delete('/:user_id', async function (req, res) {
+//     var user_id = req.params.user_id;
+//     try {
+//         var query = {
+//             $and: [{ _id: req.body._id }, { host_list: user_id }]
+//         }
+//         await EventModel.remove(query);
+//         //TODO notify on delete
+//         res.status(200);
+//     } catch (err) {
+//         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
+//     }
+// });
 
-async function getEventScoreForUser(tagFreq, friends_list, event) {
-    var score = 0;
-    friends_list.forEach(friend => {
-        if (event.attendants_list.includes(friend))
-            score += 30;
-    });
-    event.tag_list.forEach(tag => {
-        if (tagFreq[tag])
-            score += 5 * tagFreq[tag];
-    });
-    return score;
-}
+// // // Get suggested event for user
+// router.get('/suggest/:user_id', async function (req, res) {
+//     var user_id = req.params.user_id;
+//     try {
+//         var user = await UserModel.findOne({ facebook_id: user_id }).populate(attended_events_list);
+//         var tagFreq = {};
+//         user.attended_events_list.forEach(event => {
+//             event.tag_list.forEach(tag => {
+//                 if (!tagFreq[tag])
+//                     tagFreq[tag] = 0;
+//                 tagFreq[tag] = tagFreq[tag] + 1;
+//             });
+//         });
+//         var events = getVisibleEventsForUser();
+//         if (events.length == 0)
+//             res.status(409).send({ success: false, message: "no events for you" });
+//         var best_event = events[0];
+//         var best_score = getEventScoreForUser(tagFreq, user.friends_list, events[0])
+//         for (var i = 1; i < events.length; i++) {
+//             var score = getEventScoreForUser(tagFreq, user.friends_list, events[i]);
+//             if (score > best_score) {
+//                 best_score = score;
+//                 best_event = events[i];
+//             }
+//         }
+//         res.status(200).json(best_event);
+//     } catch (err) {
+//         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
+//     }
+// });
 
-async function getVisibleEventsForUser(user_id) {
-    var query = {
-        $or: [{ visibility: true }, { guest_list: user_id },
-        { host_list: user_id }
-        ]
-    }
-    return await EventModel.find(query);
-}
+// // Get all events for user, used in browse events
+// router.get('/:user_id', function (req, res) {
+//     var user_id = req.params.user_id;
+//     try {
+//         var events = getVisibleEventsForUser(user_id);
+//         res.status(200).json(events);
+//     } catch (err) {
+//         res.status(err.code >= 100 && err.code < 600 ? err.code : 500).send({ success: false, message: err.message });
+//     }
+// });
+
+// async function getEventScoreForUser(tagFreq, friends_list, event) {
+//     var score = 0;
+//     friends_list.forEach(friend => {
+//         if (event.attendants_list.includes(friend))
+//             score += 30;
+//     });
+//     event.tag_list.forEach(tag => {
+//         if (tagFreq[tag])
+//             score += 5 * tagFreq[tag];
+//     });
+//     return score;
+// }
+
+// async function getVisibleEventsForUser(user_id) {
+//     var query = {
+//         $or: [{ visibility: true }, { guest_list: user_id },
+//         { host_list: user_id }
+//         ]
+//     }
+//     return await EventModel.find(query);
+// }
 
 module.exports = router;

1a65d362b09e5083bfe70d53a54734b6341bf09f
Leslie Cheng <leslie.cheng5@gmail.com>
2019-11-03 16:48:42 -0800

    Address ESLint over-sensitivity wrt fs-non-literal

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.use((err, req, res, next) => {
 https
   .createServer(
     {
+      // eslint-disable-next-line security/detect-non-literal-fs-filename
       key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      // eslint-disable-next-line security/detect-non-literal-fs-filename
       cert: fs.readFileSync(process.env.SSL_CERT_PATH)
     },
     app

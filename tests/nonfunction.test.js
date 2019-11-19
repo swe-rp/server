81ac6d2ae4ea3789616d431afd983774b9a0b63d
Leslie Cheng <leslie.cheng5@gmail.com>
2019-11-19 01:59:03 -0800

    Added an extra second to the timeout.

--- tests/nonfunction.test.js
+++ tests/nonfunction.test.js
@@ -10,6 +10,7 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+jest.setTimeout(6000);
 
 describe("non function requirements", () => {
   let db;

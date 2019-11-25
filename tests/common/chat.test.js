a89319b7f40eb6f1fec4deb234d6fefba17a565b
Alberto Misail <ajmisailguber@gmail.com>
2019-11-24 20:36:19 -0800

    Stubs for chat tests

---
+++ tests/common/chat.test.js
@@ -0,0 +1,36 @@
+
+describe("chat", () => {
+    describe("Write message", () => {
+        test("Write message success", async () => {
+
+        });
+
+        test("Write message for unexisting user", async () => {
+
+        });
+
+        test("Write message for unexisting event", async () => {
+
+        });
+    });
+
+    describe("Handle message", () => {
+        test("Handle message for unexisting user", async () => {
+
+        });
+
+        test("Handle message for unexisting event", async () => {
+
+        });
+    });
+
+    describe("Get chat history", () => {
+        test("Get chat history success", () => {
+
+        });
+
+        test("Get chat history for unexisting event", () => {
+
+        });
+    });
+});

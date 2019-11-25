4c29ba8943616b0773f762c62a1963ec740275a7
Leslie Cheng <leslie.cheng5@gmail.com>
2019-11-24 16:30:24 -0800

    Removed unnecessary files.

--- models/tag.js
+++
@@ -1,9 +0,0 @@
-const modelNames = require("./shared");
-const mongoose = require("mongoose");
-
-let tagSchema = new mongoose.Schema({
-  name: { type: String, required: true }
-  // eventList: [{ type: mongoose.Schema.Types.ObjectId, ref: modelNames.EVENT} ]
-});
-
-module.exports = mongoose.model(modelNames.TAG, tagSchema);
